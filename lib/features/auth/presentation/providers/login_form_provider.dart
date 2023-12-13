be958476d8e37a3b205edc1b6a9918c4cd871ed4
Francisco Lino <franksuriel_7@hotmail.com>
2023-12-13 03:19:49 -0600

    FIX save state

--- lib/features/auth/presentation/providers/login_form_provider.dart
+++ lib/features/auth/presentation/providers/login_form_provider.dart
@@ -33,7 +33,9 @@ class LoginFormNotifier extends StateNotifier<LoginFormState> {
   onFormSubmit() async {
     _touchEveryField();
     if (!state.isValid) return;
+    state = state.copyWith(isPosting: false);
     await loginUserCallback(state.email.value, state.password.value);
+    state = state.copyWith(isPosting: false);
     print(state);
   }
 
