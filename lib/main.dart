56eda883e04962d23c95b4f49ce4ef374ac75ac7
Francisco Lino <franksuriel_7@hotmail.com>
2023-12-13 03:20:28 -0600

    UPDATE convert to consumer widget main app

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:teslo_shop/config/config.dart';
 
 void main() async {
-  Environment.initEnvironment();
+  await Environment.initEnvironment();
   runApp(const ProviderScope(
     child: MainApp(),
   ));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appRouter = ref.watch(goRouterProvider);
     return MaterialApp.router(
       routerConfig: appRouter,
       theme: AppTheme().getTheme(),
