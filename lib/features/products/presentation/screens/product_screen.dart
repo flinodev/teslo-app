3cbd99ec8d21f5b39b5b119450be93fa1d5831fd
Francisco Lino <franksuriel_7@hotmail.com>
2023-12-27 23:40:11 -0600

    ADD screens

---
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/products/presentation/providers/providers.dart';
+
+class ProductScreen extends ConsumerWidget {
+  final String productId;
+  const ProductScreen({super.key, required this.productId});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productState = ref.watch(productProvider(productId));
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Editar Producto'), actions: [
+        IconButton(
+            onPressed: () {}, icon: const Icon(Icons.camera_alt_outlined)),
+      ]),
+      body: Center(child: Text(productState.product?.title ?? 'Cargando')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.save_as_outlined),
+      ),
+    );
+  }
+}
